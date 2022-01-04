196f980ecf91f99174118f56965219cc0cb5c72f
supersaiyansubtlety <supersaiyansubtlety@gmail.com>
2022-01-03 21:18:05 -0800

    Implemented tag checking for leaves - better mod compat for mods that add trees with more than one leaf type - future-proofed for 1.17+ azalea leaves + flowering azalea leaves

--- src/main/java/tfar/leafmealone/MixinHooks.java
+++ src/main/java/tfar/leafmealone/MixinHooks.java
@@ -1,7 +1,34 @@
 package tfar.leafmealone;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MixinHooks {
 	public static Block block;
+
+	private static final Map<Block, Tag<Block>> LEAVES_TAGS = new HashMap<>();
+
+	public static Tag<Block> getOrSetLeavesTag(ServerWorld world, Block leavesBlock) {
+		Tag<Block> leavesTag = LEAVES_TAGS.get(leavesBlock);
+		if (leavesTag == null) {
+			String idString = Registry.BLOCK.getId(leavesBlock).toString();
+			if (!idString.endsWith("s")) idString += "s";
+			leavesTag = world.getTagManager().blocks().get(new Identifier(idString));
+			LEAVES_TAGS.put(leavesBlock, leavesTag);
+		}
+
+		return leavesTag;
+	}
+
+	public static Tag<Block> getLeavesTag(Block leavesBlock) {
+		return LEAVES_TAGS.get(leavesBlock);
+	}
+
 }
--- src/main/java/tfar/leafmealone/mixin/MixinLeavesBlock.java
+++ src/main/java/tfar/leafmealone/mixin/MixinLeavesBlock.java
@@ -4,8 +4,8 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LeavesBlock;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.tag.Tag;
 import net.minecraft.util.math.BlockPos;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Group;
@@ -16,6 +16,9 @@ import tfar.leafmealone.MixinHooks;
 
 import java.util.Random;
 
+import static tfar.leafmealone.MixinHooks.getLeavesTag;
+import static tfar.leafmealone.MixinHooks.getOrSetLeavesTag;
+
 @Mixin(LeavesBlock.class)
 abstract class MixinLeavesBlock extends Block {
 	public MixinLeavesBlock(Settings settings) {
@@ -25,18 +28,25 @@ abstract class MixinLeavesBlock extends Block {
 
 	@Inject(method = "scheduledTick",at = @At(value = "HEAD"))
 	private void captureBlock(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci){
+		getOrSetLeavesTag(world, this);
 		MixinHooks.block = this;
 	}
 
 	@Group(name = "LeavesBlockInstanceofRedirects")
 	@Redirect(method = "getDistanceFromLog", at = @At(value = "CONSTANT", args = "classValue=net/minecraft/block/LeavesBlock"))
-	private static boolean strictCheckDev(Object blockOfState, Class<?> leavesBlockClass) {
-		return blockOfState == MixinHooks.block;
+	private static boolean strictLeavesCheckDev(Object stateBlock, Class<?> leavesBlockClass) {
+		return strictLeavesCheckImpl(stateBlock);
 	}
 
 	@Group(name = "LeavesBlockInstanceofRedirects")
 	@Redirect(method = "getDistanceFromLog", at = @At(value = "CONSTANT", args = "classValue=net/minecraft/block/net/minecraft/class_2397"))
-	private static boolean strictCheckProd(Object blockOfState, Class<?> leavesBlockClass) {
-		return blockOfState == MixinHooks.block;
+	private static boolean strictLeavesCheckProd(Object blockOfState, Class<?> leavesBlockClass) {
+		return strictLeavesCheckImpl(blockOfState);
+	}
+
+	private static boolean strictLeavesCheckImpl(Object stateBlock) {
+		Tag<Block> leavesTag = getLeavesTag(MixinHooks.block);
+		return leavesTag != null && leavesTag.contains((Block)stateBlock) ||
+				stateBlock == MixinHooks.block;
 	}
 }
